c6a686b4c77f3520851f787fdfec73da2d74e5f8
Forest wilson <hexaforest@gmail.com>
2021-03-16 23:40:36 -0400

    created functions to build questions and call from a questions array

--- practice/script.js
+++ practice/script.js
@@ -1,42 +1,125 @@
-const quizCont = document.getElementById("container");
+const questionCont = document.getElementById("questionContainer");
 const question = document.getElementById("questionText");
 const startButton = document.getElementById("start");
-const answCont = document.getElementById("answers");
-const title = document.getElementById("title");
+const answCont = document.getElementById("answerButtons");
+const startBtn = document.getElementById("startButton");
+const nextBtn = document.getElementById("nextButton");
+
+let index = 0;
 
 
-const btn = document.createElement("button");
+
+startBtn.addEventListener("click", startGame);
+
+function startGame() {
+    console.log("hello");
+    startBtn.classList.add("hide");
+    nextBtn.classList.remove("hide");
+
+    let questionEl = document.createElement("div");
+    let answerGrid = document.createElement("div");
+    
+    questionEl.innerHTML = questions[0].question;
+
+    questionCont.append(questionEl);
+    questionCont.append(answerGrid);
+    
+    createAnswers();
+}
+
+function createAnswers() {
+    for (let i = 0; i < 4; i++) {
+        let answerBtn = document.createElement("button")
+        answerBtn.innerText = questions[index].answers[i];
+        questionCont.append(answerBtn);
+        
+    }
+}
+
+nextBtn.addEventListener("click", setNextQuestion);
+
+function setNextQuestion() {
+    
+    index++;
+    let questionEl = document.createElement("div");
+    let answerGrid = document.createElement("div");
+
+    questionEl.innerText = questions[index].question;
+    questionCont.append(questionEl);
+    questionCont.append(answerGrid);
+    createAnswers(index);
+
+}
+
+
+
+
+
+
+function selectAnswer() {
+
+}
+
 
 const questions = [
     {
-        question1: "who was the best president?",
-        answers: {
-            a: "James buchanan",
-            b: "George Bush",
-            c: "the other Geroge Bush",
-            d: "monicaaaaaaa"
+        question: "who was the best president?",
+        answers: [
+            "James buchanan",
+            "George Bush",
+            "the other Geroge Bush",
+            "monicaaaaaaa"
+        ],
+        correctAnswer: 0
+    
     },
-    correctAnswer: "a"
-}
+    {
+        question: "what is my fav food?",
+        answers: [
+            "chip",
+            "burger",
+            "fry",
+            "hut dog"
+        ],
+        correctAnswer: 2
 
-
-
-
-startButton.addEventListener("click", function(event) {
-    event.preventDefault();
-    title.remove();
-    question.innerHTML = questions.question1;
-    startButton.innerHTML = "submit";
-    createQuiz();
-
-})
-
-
-
-let createQuiz = function() {
-    for (let i = 0; i < 5; i++) {
-        answCont.append(btn);
-        btn.innerText = questions.answers[i.valueOf];
-        return;
+    },
+    {
+        question: "what is the first code?",
+        answers: [
+            "010101",
+            "HTML",
+            "ATML",
+            "C+++++++++"
+        ],
+        correctAnswer: 1
     }
-}
+]
+
+
+
+// const question = questions[pointer];
+// buildQuestion(question);
+
+// onClick = {handleClick}
+// onClick = {() => handleClick(label)}
+
+
+// handle = (e) => {
+//     if (e.target.name === question[0].correctAnswer)
+// }
+
+
+// <button name="a">answer a</button>
+
+// [1,2,3].forEach((val) =>  console.log(val))
+// const t = [1,2,3].map((val) => 1)
+// [1,1,1]
+
+// answers.forEach((val, idx) => {
+//     const element = document.createElement("Button");
+//     element.innerText = val;
+//     element.name = idx;
+//     element.addEventListener("click", handleclick)
+//     //container.append(element)
+// })
