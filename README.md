3b665cc2b5a34f229ed122eb1af404428603b355
ForestW70 <hexaforest@gmail.com>
2021-03-18 18:13:33 -0400

    prepared for submission. added readme.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-## Forest Wilson - Homework 4 - Code Quiz
+# Forest Wilson - Homework 4 - Code Quiz
 
+Are you able to complete the worlds hardest coding quiz? To score with high marks, use your knowledge about types of code, code trivia, and general useless knowledge, becuase coming up with questions is hard! If you have done better than a previous session,  you can submit your highscore to display until you submit a new higher score.
+
+## Comments
+
+Writing out the JS to this one took much longer than I thought it would be so there were some visual aspects that I wanted to include such as indicators telling you you're correct and a more descriptive end screen if you did not win or get highscore, but I'm happy with how the looks work in this application.
+
+I know that one of the critera of the homework was to be able to implement a leaderboard to which you can add your highscore + name to, but I had an issue figuring out how to submit the same type of data in the local storage but under different names, or rather storing more than one highscore without deleting another. I decided to just opt for a display highscore feature that will store and show the current highscore. If your score is higher than that it will replace the value, but if it is lower in value it does not replace.
+
+Would have also liked to add a value in the highscore object that displays time left when you submitted your highscore, but I'll have to save that for the future.
+
+<!-- note to self: do not use highscore as an attribute, especially if you cant spell. You will have a bad time. -->
+
+## bugs
+
+when you get a new score that is equivilent to your current highscore, it will replace even though it is not a higher score. something with my Number.parse opperator I imagine.
+
+## Screenshots
+
+![start screen](./assets/images/first-screen.png)
+![Active screen](./assets/images/quiz-in-progress.png)
+
+## Relevant links
+
+[live site](https://forestw70.github.io/hw4codequiz/)
+[repo](https://github.com/ForestW70/hw4codequiz)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,326 @@
-const quizBlock = document.getElementById("quiz-container");
-const quizEl = document.getElementById("quiz-container").children[0];
-const highscoreBlock = document.getElementById("highscore-container");
-const timerBlock = document.getElementById("timer-container");
-const startButton = document.getElementById("start-button");
+// defining my paths by getting ID's
+const questionCont = document.getElementById("questionContainer");
+const startBtn = document.getElementById("startButton");
+const nextBtn = document.getElementById("nextButton");
+const scoreValue = document.getElementById("score");
+const timerValue = document.getElementById("timer");
+const intro = document.getElementById("greeting");
+const progressCont = document.getElementById("quizProgress");
+const hiscoreCont = document.getElementById("hiscore");
 
 
-startButton.addEventListener('click', function(event) {
-    event.preventDefault();
-    quizEl.style.visibility = "hidden";
-    createQuiz();
-});
+// setting up variables that will be tracked and used later on
+let index = 0;
+let score = 0;
+let timer = 75;
+let isCorrect = false;
+let interval;
 
-function createQuiz() {
-    let body = quizEl.createElement("div");
-    let question = quizEl.children[0].createElement("p");
-    body.appendChild();
-    question.appendChild();
-    // let question = quizEl.children[0].children[0];
-    question.innerHTML("hey how are ya");
-};
+
+// click event for my start button.
+startBtn.addEventListener("click", startGame);
+
+
+// start game function. shows and hides elements that I want for when the game begins.
+// starts my interval by setting it equal to my timer function.
+function startGame() {
+    intro.classList.add("hide");
+    startBtn.classList.add("hide");
+    nextBtn.classList.remove("hide");
+    progressCont.classList.remove("hide");
+    createAnswers();
+    interval = window.setInterval(timerFunk, 1000) 
+}
+
+
+// sets timer to decriment by 1 every 1000ms (above).
+// when displaying timer, set it to timer value if greater than 0, if it isnt, set timer to 0.
+// (functionality: stops timer from displaying negative values.)
+// if timer is less than or equal to 0, run highscore screen. stop timer from ticking in background.
+function timerFunk() {
+    timer--;
+    timerValue.innerText = timer > 0 ? timer : 0;
+    if (timer <= 0) {
+        createEndScreen();
+        clearInterval(interval);
+    }
+}
+
+
+// function to build question and answers. creates container divs and gives them appropriate classes.
+// sets my question to the question at index value (0 here) of my questions objecdt.
+// append containers, then run a for loop to itterate and create my answer buttons in a similar fashion.
+// created a click handeler for my answer grid continer.
+function createAnswers() {
+    let questionEl = document.createElement("div");
+    let answerGrid = document.createElement("div");
+    questionEl.setAttribute("class", "question");
+    answerGrid.setAttribute("class", "answer-grid");
+    questionEl.innerHTML = questions[index].question;
+    questionCont.append(questionEl);
+    questionCont.append(answerGrid);
+    
+    for (let i = 0; i < 4; i++) {
+        let answerBtn = document.createElement("button")
+        answerBtn.innerText = questions[index].answers[i];
+
+        // sets name of element equal to its i value when created. will be important when checking if correct.
+        answerBtn.setAttribute("name", i);
+        answerBtn.setAttribute("class", "btn");
+        answerBtn.classList.add("hover-effect");
+        answerGrid.append(answerBtn);
+        answerBtn.addEventListener("click", handleClick);
+    }
+    
+}
+
+
+// function to handle how clicking answers work.
+// checks to see if clicked button's name corrosponds with its correct value in the questions object.
+function handleClick (e) {
+    
+
+    // if target button's name is euqal to value of correct answer in it's question array, run "correct" script.
+    // (add class to button to display green, change isCorrect value, add 100 to score, disable selection of other answers.)
+    // if target it not equal, lose 5 time, set button to display red.
+    // (similar time function to ensure timer subtraction updates realtime).
+    if (e.target.name == questions[index].correctAnswer) {
+        e.target.classList.add("correct");
+        isCorrect = true;
+        score += 100;
+        scoreValue.innerText = score;
+        questionCont.querySelectorAll("button").forEach(function(val) {
+            val.setAttribute("disabled", true);
+        });
+
+    } else {
+        timer -= 5; 
+        e.target.classList.add("incorrect");
+        timerValue.innerText = timer > 0 ? timer : 0;
+    }
+    
+    
+}
+
+
+// click event for next button. used to cycle through question slides.
+nextBtn.addEventListener("click", setNextQuestion);
+
+
+// if index is about to enter position in questions that does not exist, run end screen function.
+// if not and isCorrect is true, clear the question container, incriment index, load create answers function using index value as argument.
+// reset isCorrect for next question.
+function setNextQuestion() {
+    if (index + 1 === questions.length) {
+        createEndScreen();
+    
+    } else if (isCorrect) {
+        questionCont.innerHTML = '';
+        index++;
+        createAnswers(index);
+        isCorrect = false;
+    }
+}
+
+
+// function to display screen when game is over. clears question container. hides some elements.
+// create needed elements. assigns all values. display final messages. create input field for player. append all elements.
+// add click event for hiscore submission.
+function createEndScreen() {
+    questionCont.innerHTML = '';
+    nextBtn.classList.add("hide");
+    progressCont.classList.add("hide");
+
+    let endScreen = document.createElement("div");
+    let finalMessage = document.createElement("h2");
+    let finalScore = document.createElement("span");
+    let hiscoreLabel = document.createElement("label");
+    let hiscoreInput = document.createElement("input");
+    let submit = document.createElement("input");
+
+    endScreen.setAttribute("class", "end-screen");
+    hiscoreLabel.setAttribute("for", "hiscore");
+    hiscoreInput.setAttribute("type", "text");
+    hiscoreInput.setAttribute("id", "hiscoreName");
+    submit.setAttribute("type", "submit");
+    submit.setAttribute("value", "Submit score and try again");
+
+
+    finalMessage.innerText = "Thanks for playing! Hopefully it wasn't too hard ;)";
+    finalScore.innerHTML = `Your final score is: <strong>${score}</strong>`;
+    hiscoreLabel.innerText = "Enter your initials here to submit your score.";
+    
+
+    questionCont.append(endScreen);
+    endScreen.append(finalMessage);
+    endScreen.append(finalScore);
+    endScreen.append(hiscoreLabel);
+    endScreen.append(hiscoreInput);
+    endScreen.append(submit);
+
+    submit.addEventListener("click", setHiscore);
+}
+
+
+// this is how I set hiscore using local storage.
+// set variable to a parsed form of our current hiscore, then run an if loop to check if either a highscore has not been submitted, or current score is greater than our stored score.
+// sets up object to store data locally, adds two values to objects if true.
+// after submit, reload page to play again.
+function setHiscore() {
+
+    const highScore = JSON.parse(localStorage.getItem("currentHiscore"));
+
+    if (!highScore || score > Number.parseInt(highScore.score)) {
+        let currentHiscore = {
+            name: document.getElementById("hiscoreName").value,
+            score: score
+        };
+
+        localStorage.setItem("currentHiscore", JSON.stringify(currentHiscore));
+    }
+    location.reload();
+}
+
+// this will check on startup if there is anything logged to local storage, if so, show hiscore container and populate it with the current highscore values.
+if (localStorage.getItem("currentHiscore")) {
+    hiscoreCont.setAttribute("class", "you-did-it");
+    let showName = JSON.parse(localStorage.getItem("currentHiscore")).name;
+    let showScore = JSON.parse(localStorage.getItem("currentHiscore")).score;
+    document.getElementById("hiscore").innerHTML = `<span>Current Hiscore:</span><h2>${showName}</h2><span>score: ${showScore}</span>`;
+}
+
+
+// Qustions object where my quiz will pull all questions, asnwers, and correct answer values.
+// correctAnswer is set = to the right answers position in the answers array. the name of the buttons element created above helps us target the correct answer,
+// becuase the buttons will populate in linear way, it lines up value wise with this array.
+const questions = [
+    {
+        question: "What does the 'h' stand for in HTML?",
+        answers: [
+            "Hyper",
+            "Hearty",
+            "Herman's",
+            "Head"
+        ],
+        correctAnswer: 0
+    
+    },
+    {
+        question: "What is the first indexed value of an array in JavaScript?",
+        answers: [
+            "1",
+            "-1",
+            "0",
+            "3 fitty"
+        ],
+        correctAnswer: 2
+
+    },
+    {
+        question: "What type of language is CSS?",
+        answers: [
+            "Programming language",
+            "Style Sheet language",
+            "Markup language",
+            "French"
+        ],
+        correctAnswer: 1
+    },
+    {
+        question: "Which of the following is not a valid 'type' when creating an event listener?",
+        answers: [
+            "Click",
+            "Mouse Up",
+            "Key Down",
+            "Clack"
+        ],
+        correctAnswer: 3
+    
+    },
+    {
+        question: "What port does a browser assume when you use an https link?",
+        answers: [
+            "1",
+            "80",
+            "443",
+            "5000"
+        ],
+        correctAnswer: 2
+
+    },
+    {
+        question: "Who is the inventor of Git?",
+        answers: [
+            "Lief Tortelini",
+            "Linus Torvalds",
+            "Tinus Lorvalds",
+            "Besteman Besteman Besteman"
+        ],
+        correctAnswer: 1
+    },
+    {
+        question: "Which of the following is not an HTML element?",
+        answers: [
+            "<heading>",
+            "<legend>",
+            "<tr>",
+            "<sup>"
+        ],
+        correctAnswer: 0
+    
+    },
+    {
+        question: "What are the values called that you use inside of a JavaScript function()?",
+        answers: [
+            "Variables",
+            "Values",
+            "Arguments",
+            "Squabbles"
+        ],
+        correctAnswer: 2
+
+    },
+    {
+        question: "Which of the following features can not be found in Chrome's inspect feature?",
+        answers: [
+            "Elements",
+            "Free Parking",
+            "Console",
+            "Sources"
+        ],
+        correctAnswer: 1
+    },
+    {
+        question: "What is the most bang for your buck?",
+        answers: [
+            "Musk's NeuroLink",
+            "1654 years for a PHD",
+            "CS Degree",
+            "Coding Bootcamp"
+        ],
+        correctAnswer: 3
+    
+    },
+    {
+        question: "Who said 'Never trust a computer you can't throw out a window'?",
+        answers: [
+            "Steve Irwin",
+            "Steve Jobs",
+            "Steve Wozniak",
+            "Steve O"
+        ],
+        correctAnswer: 2
+
+    },
+    {
+        // you better get this one correct 🔫
+        question: "How sick was this quiz????",
+        answers: [
+            "Ehh",
+            "Sooooo sick bro",
+            "Not Sick",
+            "Get this right of prepare 4 hax"
+        ],
+        correctAnswer: 1
+    }
+]
